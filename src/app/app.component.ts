27274db1fe71657f2350a34513ce6ffc81e6f936
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-09-12 15:08:55 -0300

    Implementado el listar en la vista html component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from "@angular/common";
+import { ProductComponent } from './products/components/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
