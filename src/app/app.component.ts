a17e1033a26eba54f155da7edb3ba6060a09c391
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-09-12 12:04:48 -0300

    La Directiva ngFor y nueva sintaxis de flujos de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from "@angular/common";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title: string = "Hola mundo en angular";
   enabled: boolean = false;
+  courses: string[] = ['Angular', 'React', 'Spring Boot'];
   setEnabled(): void {
     this.enabled = this.enabled ? false : true;
   }
